af80b1fb9cdef44dc9136f7f2665532ded56e789
Arjun Panicker <arjunpanicker95@gmail.com>
2023-09-20 20:31:45 +0530

    Implement state change

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
+import 'package:roll_dice/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({required this.colors, super.key});
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(BuildContext context) {
@@ -18,16 +16,9 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-          child: Column(
-        children: [
-          Image.asset(
-            'assets/images/dice-1.png',
-            width: 200,
-          ),
-          TextButton(onPressed: rollDice, child: const StyledText('Randomize'))
-        ],
-      )),
+      child: const Center(
+        child: DiceRoller(),
+      ),
     );
   }
 }

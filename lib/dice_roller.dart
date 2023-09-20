29457ec2633b6dd5b9d1bae5e68b56ce82f4182d
Arjun Panicker <arjunpanicker95@gmail.com>
2023-09-20 20:38:14 +0530

    Dice Roller complete

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
+import 'dart:math';
+
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
@@ -11,11 +13,12 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-1.png';
+  final randomizer = Random();
+  var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1; // 1 <> 6
     });
   }
 
@@ -25,7 +28,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(
